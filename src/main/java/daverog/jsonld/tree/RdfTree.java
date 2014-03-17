1b4261636c91cdc88489944855acf4d58e303da7
Clynt Pinfold <clynt.pinfold@bbc.co.uk>
2014-03-17 09:43:13 +0000

    Clean up whitespace.

--- src/main/java/daverog/jsonld/tree/RdfTree.java
+++ src/main/java/daverog/jsonld/tree/RdfTree.java
@@ -13,7 +13,7 @@ import com.hp.hpl.jena.rdf.model.Statement;
 
 
 public class RdfTree implements Comparable<RdfTree> {
-	
+
 	public static final String DEFAULT_RESULT_ONTOLOGY_URI_PREFIX = "http://purl.org/ontology/rdf-result/";
 	public static final String RDF_PREFIX = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
 	public static final String RDF_TYPE = RDF_PREFIX + "type";
@@ -21,7 +21,7 @@ public class RdfTree implements Comparable<RdfTree> {
 
 	private final RdfTree parent;
 	private List<RdfTree> children = Lists.newArrayList();
-	
+
 	private final boolean inverse;
 	private final boolean list;
 	private final RDFNode node;
@@ -29,9 +29,9 @@ public class RdfTree implements Comparable<RdfTree> {
 	private final Model model;
 	private final NameResolver nameResolver;
 	private Resource type;
-  HashMap<RDFNode, Integer> mapFromChildToDepth = null;
+	HashMap<RDFNode, Integer> mapFromChildToDepth = null;
 	private boolean constructed = false;
-	
+
 	public RdfTree(Model model, NameResolver nameResolver, RdfTree parent, RDFNode node, Property predicate, boolean inverse, HashMap<RDFNode, Integer> mapFromChildToDepth) {
 		this.model = model;
 		this.nameResolver = nameResolver;
@@ -39,16 +39,16 @@ public class RdfTree implements Comparable<RdfTree> {
 		this.node = node;
 		this.predicate = predicate;
 		this.inverse = inverse;
-    this.mapFromChildToDepth = mapFromChildToDepth;
+		this.mapFromChildToDepth = mapFromChildToDepth;
 		list = false;
 	}
-	
+
 	public RdfTree(Model model, NameResolver nameResolver, RDFNode rootNode, HashMap<RDFNode, Integer> mapFromChildToDepth) {
 		this.model = model;
 		this.nameResolver = nameResolver;
 		this.node = rootNode;
-    this.mapFromChildToDepth = mapFromChildToDepth;
-    list = false;
+		this.mapFromChildToDepth = mapFromChildToDepth;
+		list = false;
 		predicate = null;
 		inverse = false;
 		parent = null;
@@ -57,9 +57,9 @@ public class RdfTree implements Comparable<RdfTree> {
 	public RdfTree(Model model, NameResolver nameResolver, HashMap<RDFNode, Integer> mapFromChildToDepth) {
 		this.model = model;
 		this.nameResolver = nameResolver;
-    this.mapFromChildToDepth = mapFromChildToDepth;
+		this.mapFromChildToDepth = mapFromChildToDepth;
 		list = true;
-    predicate = null;
+		predicate = null;
 		inverse = false;
 		parent = null;
 		node = null;
@@ -72,9 +72,9 @@ public class RdfTree implements Comparable<RdfTree> {
 			childNode = statement.getObject();
 			inverse = false;
 		}
-		
-		//Rule 1: Do not follow inverse type predicates. 
-		//This prevents commonly typed resources in a graph from creating overly large trees
+
+		//Rule 1: Do not follow inverse type predicates.
+		//This prevents commonly typed resources in a graph from creating overly large tree
 		if (inverse && statement.getPredicate().getURI().equals(RDF_TYPE)) return;
 
 		//Rule 2: If a node is present as a parent node, do not continue with more children
@@ -87,30 +87,30 @@ public class RdfTree implements Comparable<RdfTree> {
 		//Rule 4: Do not follow the inverse of properties just followed if they lead to nodes that are list items
 		//This prevents 'reference data' from forming join-points in RDF lists
 		if (isInverse() != inverse && parent != null && getPredicate() != null &&
-        getPredicate().equals(statement.getPredicate()) && hasListRootWithNode(childNode)) return;
+				getPredicate().equals(statement.getPredicate()) && hasListRootWithNode(childNode)) return;
 
 		//Rule 5: Do not follow inverse properties if they lead to nodes that are
 		//        closer to the root (but not necessarily a parent)
 		//This prevents 'reference data' from forming join-points in RDF lists
-    if (inverse && parent != null && getPredicate() != null) {
-      int depthOfPotentialChild = getDepth() + 1;
-      boolean hasSiblingOrAncestorSibling = depthOfPotentialChild >= getDepthInTree(node);
-      if (hasSiblingOrAncestorSibling) return;
-    }
+		if (inverse && parent != null && getPredicate() != null) {
+			int depthOfPotentialChild = getDepth() + 1;
+			boolean hasSiblingOrAncestorSibling = depthOfPotentialChild >= getDepthInTree(node);
+			if (hasSiblingOrAncestorSibling) return;
+		}
 
-    RdfTree rdfTree = new RdfTree(model, nameResolver, this, childNode, statement.getPredicate(), inverse, mapFromChildToDepth);
-    children.add(rdfTree);
-    mapFromChildToDepth.put(rdfTree.getNode(), getDepth());
-  }
+		RdfTree rdfTree = new RdfTree(model, nameResolver, this, childNode, statement.getPredicate(), inverse, mapFromChildToDepth);
+		children.add(rdfTree);
+		mapFromChildToDepth.put(rdfTree.getNode(), getDepth());
+	}
 
-  public void addListItem(Resource listItem) {
+	public void addListItem(Resource listItem) {
 		children.add(new RdfTree(model, nameResolver, this, listItem, null, false, mapFromChildToDepth));
 	}
 
 	public Property getPredicate() {
 		return predicate;
 	}
-	
+
 	public RDFNode getNode() {
 		return node;
 	}
@@ -119,7 +119,7 @@ public class RdfTree implements Comparable<RdfTree> {
 	public List<RdfTree> getChildren() {
 		return children;
 	}
-	
+
 	public DirectionalPredicate getDirectionalPredicate() {
 		return new DirectionalPredicate(getPredicate(), inverse);
 	}
@@ -128,37 +128,37 @@ public class RdfTree implements Comparable<RdfTree> {
 		this.type = type;
 	}
 
-  public boolean hasParentWithNode(RDFNode node) {
-    if (parent == null) return false;
-    if (parent.getNode() == null) return false;
-    if (parent.getNode().equals(node)) return true;
-    return parent.hasParentWithNode(node);
-  }
+	public boolean hasParentWithNode(RDFNode node) {
+		if (parent == null) return false;
+		if (parent.getNode() == null) return false;
+		if (parent.getNode().equals(node)) return true;
+		return parent.hasParentWithNode(node);
+	}
 
-  public boolean hasListRootWithNode(RDFNode node) {
+	public boolean hasListRootWithNode(RDFNode node) {
 		if (parent == null) return false;
 		if (parent.isList()) return parent.hasListItemWithNode(node);
 		return parent.hasListRootWithNode(node);
 	}
 
-  private int getDepthInTree(RDFNode node) {
-    return getRoot().getDepthOfNode(node);
-  }
+	private int getDepthInTree(RDFNode node) {
+		return getRoot().getDepthOfNode(node);
+	}
 
-  private int getDepthOfNode(RDFNode nodeArg) {
-    if (node != null && node.equals(nodeArg)) return 0;
-    Integer depth = mapFromChildToDepth.get(nodeArg);
-    if (depth == null) return -1;
-    return depth;
-  }
+	private int getDepthOfNode(RDFNode nodeArg) {
+		if (node != null && node.equals(nodeArg)) return 0;
+		Integer depth = mapFromChildToDepth.get(nodeArg);
+		if (depth == null) return -1;
+		return depth;
+	}
 
-  private RdfTree getRoot() {
+	private RdfTree getRoot() {
 		if (parent == null) return this;
 		return parent.getRoot();
 	}
 
 	/**
-	 * Returns 0 for the list, 1 for each tree root, 
+	 * Returns 0 for the list, 1 for each tree root,
 	 * and +1 for each child thereafter
 	 */
 	private int getDepth() {
@@ -181,7 +181,6 @@ public class RdfTree implements Comparable<RdfTree> {
 		}
 		if (!list) Collections.sort(children);
 	}
-
 
 	private boolean isLiteral() {
 		return getNode().isLiteral();
@@ -211,12 +210,12 @@ public class RdfTree implements Comparable<RdfTree> {
 
 		if (isLiteral() && !tree.isLiteral()) return -1;
 		if (!isLiteral() && tree.isLiteral()) return 1;
-		
+
 		if (isChildlessResource() && !tree.isChildlessResource()) return -1;
 		if (!isChildlessResource() && tree.isChildlessResource()) return 1;
-		
+
 		if (getPredicate().equals(tree.getPredicate())) {
-			if (isLiteral() && tree.isLiteral()) 
+			if (isLiteral() && tree.isLiteral())
 				return RdfTreeUtils.compareObjects(
 						getNode().asLiteral().getValue(),
 						tree.getNode().asLiteral().getValue());
@@ -224,18 +223,18 @@ public class RdfTree implements Comparable<RdfTree> {
 					getNode(),
 					tree.getNode());
 		}
-		
+
 		return nameResolver.compareNames(getPredicate(), tree.getPredicate());
 	}
 
 	public String asXml() {
 		return new RdfTreeXmlWriter().asXml(this);
 	}
-	
+
 	public String asHtml(String relativeUrlBase) {
 		return new RdfTreeXmlWriter().asHtml(this, relativeUrlBase);
 	}
-	
+
 	public String asJson() {
 		return new RdfTreeJsonWriter().asJson(this);
 	}
@@ -263,14 +262,14 @@ public class RdfTree implements Comparable<RdfTree> {
 	/**
 	 * Get the 'depth' of the resource,
 	 * indicated by the URI, in this tree.
-	 * 
+	 *
 	 * 0 = The root node matches this URI
 	 * 1 = One of the tree's immediate child nodes
 	 *     matches the URI
 	 * 2 = Grandchildren
-	 * 
+	 *
 	 * etc..
-	 * 
+	 *
 	 * This is a relative depth, rather
 	 * than an absolute depth, where it makes
 	 * no difference whether this tree is the root
@@ -297,16 +296,16 @@ public class RdfTree implements Comparable<RdfTree> {
 	 * To be 'constructed' means that, based on the graph,
 	 * every child tree or literal value has been identified
 	 * based on the triple statements.
-	 * 
-	 * It does not necessarily mean that the children 
+	 *
+	 * It does not necessarily mean that the children
 	 * themselves have been 'constructed'.
 	 */
 	public boolean isConstructed() {
 		return constructed;
 	}
-	
+
 	public void markAsConstructed() {
 		constructed = true;
 	}
-	
+
 }
